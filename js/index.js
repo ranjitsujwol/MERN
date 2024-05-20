4cd6ea331ac14c526440f2c81439c1ea4f12aed3
ranjitsujwol <59633138+ranjitsujwol@users.noreply.github.com>
2024-05-20 15:07:14 +0530

    JS: Variables

---
+++ js/index.js
@@ -0,0 +1,9 @@
+greetings= "hi"
+console.log(greetings, "ram");
+console.log(greetings, "shyam");
+console.log(greetings, "hari");
+console.log(greetings, "laxman");
+console.log(greetings, "sita");
+console.log(greetings, "shreya");
+
+
